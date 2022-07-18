76f2fffc7364c9205550b230644fe705f88b4f70
Craig Gregory <craig.jonathan.gregory@gmail.com>
2022-07-18 12:08:51 -0500

    Initial

--- src/main/java/xcom/selenium/HelloWorld.java
+++ src/main/java/xcom/selenium/HelloWorld.java
@@ -1,8 +1,11 @@
 package xcom.selenium;
 
+import xcom.utils4j.format.Strings;
+
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World !!!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
+		System.out.println(Strings.fillString(80, '*'));
+	}
 }
